1ca7bc9962bc986c9152c9690dee5c982767329b
jennifer hodge <jennifer.hodge@russdarrow.com>
2025-05-21 09:06:48 -0500

    nested player 1 and 2 and added timeout function

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
 
 const beginGame = () => {
   rl.question("Welcome to Dice Battle! Begin the game!", () => {
-    rl.question("Player one may start!", () => {
+    rl.question("Player one may start! Press Enter", () => {
       rollDice();
     });
   });
@@ -18,56 +18,44 @@ beginGame();
 
 let battleSoldiers = [0, 1, 2, 3, 4, 5, 6, 7];
 let startGame;
+let startGame2;
 
 const rollDice = () => {
   rl.question("Player 1, Roll your dice!", () => {
     const startGame = Math.floor(Math.random() * 6) + 1;
     console.log(`Player 1, You picked dice # ${startGame}!`);
 
-    rl.question("Player 2, Roll your dice!", () => {
-      const startGame = Math.floor(Math.random() * 6) + 1;
-      console.log(`Player 2, You picked dice # ${startGame}!`);
+    rl.question("Press enter to continue", () => {
+      rl.question("Player 2, Roll your dice!", () => {
+        const startGame2 = Math.floor(Math.random() * 6) + 1;
+        console.log(`Player 2, You picked dice # ${startGame2}!`);
 
+        rl.close();
 
-
-      
-
-      rl.close();
+        const showdownPause = () => {
+          console.log("Let the battle begin!");
+        };
+        setTimeout(showdownPause, 5000);
+      });
     });
   });
 };
 
-// let battleSoldiers = [0, 1, 2, 3, 4, 5, 6, 7];
-// let startGame;
-
-// const rollDice = () => {
-//   rl.question("Player 1, Roll your dice!", () => {
-//     const startGame = Math.floor(Math.random() * 6) + 1;
-//     console.log(`Player 1, You picked dice # ${startGame}!`);
-
-//     rl.question("Player 2, Roll your dice!", () => {
-//       const startGame = Math.floor(Math.random() * 6) + 1;
-//       console.log(`Player 2, You picked dice # ${startGame}!`);
-
-//       rl.close();
-//     });
-//   });
-// };
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-// let player1 = 0;
-// let player2 = 0;
+let player1 = 0;
+let player2 = 0;
 
-// let gameResult = 0;
+let gameResult = 0;
 
-// // const compareScores = (rollDice) => {
-// //     rl.getPrompt("Begin the game", () => {
-// //         console.log('Player one may start')
+const compareScores = (rollDice) => {
+    rl.getPrompt("Begin the game", () => {
+        console.log('Player one may start')
 
-// //     })
+    })
 
-// // }
+}
 
 // + → Addition
 // - → Subtraction
